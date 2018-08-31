10139a073797fe920f2cc3bc04556a45464296eb
azhry <arliansyah_azhary@yahoo.com>
2018-08-31 15:42:25 +0700

    Modular routing

--- app.py
+++ app.py
@@ -1,14 +1,7 @@
 from flask import Flask, render_template
+from MyRoute import MyRoute
+from YourRoute import YourRoute
+
 app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
-
-@app.route('/test/<param>')
-def test(param):
-	return 'Test %s' % param
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-	return render_template('login.html')
+app.register_blueprint(MyRoute.page)
+app.register_blueprint(YourRoute.page)
